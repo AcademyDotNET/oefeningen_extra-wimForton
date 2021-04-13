e258ee0ede2df1412f55e92e7e67dbd41157646e
wim forton <wim.forton@gmail.com>
2021-04-13 16:32:54 +0200

    enemy ufo added, rocket class added, gameloop

--- Extras/SpaceInvader/Program.cs
+++ Extras/SpaceInvader/Program.cs
@@ -6,7 +6,34 @@ namespace SpaceInvader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.CursorVisible = false;
+            EnemyUfo myEnemyUfo1 = new EnemyUfo();
+            Rocket myRocket1 = new Rocket();
+            Rocket myRocket2 = new Rocket();
+            int test = 0;
+            /*
+            for (int i = 0; i < 50; i++)
+            {
+                myEnemyUfo1.Draw();
+                System.Threading.Thread.Sleep(20);
+            }
+            */
+            ConsoleKey myKey = Console.ReadKey(true).Key;
+            //char key = 'i';
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    myEnemyUfo1.Draw();
+                    System.Threading.Thread.Sleep(1);
+                }
+            } while (myKey != ConsoleKey.Escape);
+
         }
     }
 }
